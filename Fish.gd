04413a1a3570684e12b98555359824fd4479d0b2
Ana Gelez <ana@gelez.xyz>
2023-11-09 17:56:00 +0100

    Basic implementation of the simulation
    It follows the equations provided in the base paper.
    
    For now only the feeding phase is implemented, the growing phase is not.
    The fish cannot actually eat the food either.
    
    A can be pressed to spawn some food.
    Z can be used to spawn a fish.

---
+++ Fish.gd
@@ -0,0 +1,148 @@
+extends Node3D
+
+# W in the article
+var body_mass = 1.0
+# TL in the article
+var total_length = 0.1
+# S in the article
+var feed_intake_weight = 0.0
+const MAX_FEED_INTAKE = 10.0 # TODO: actual value
+
+var swimming_force_vector = Vector3.RIGHT
+var acceleration = Vector3.RIGHT
+var speed = Vector3.ZERO
+
+enum PHASE {
+	FEEDING,
+	GROWING,
+}
+
+var current_phase = PHASE.FEEDING
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var w = [0.6, 0.4, 0.4, 1.0, 0.0, 0.2] # w1 to w6 (the index are shifted in Godot and go from 0 to 5)
+var max_speed_coeff = 1.5 # Cv in the article
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if current_phase == PHASE.FEEDING:
+		# this algorithm is based on figure 2
+		if feed_in_tank() && feed_intake_weight < MAX_FEED_INTAKE:
+			w[4] = 3.0
+			max_speed_coeff = 7.0
+		else:
+			w[4] = 0.0
+			max_speed_coeff = 1.5
+		# update swimming force vector
+		swimming_force_vector = compute_swimming_force()
+		# compute acceleration, speed and position
+		# Equations (1) in the article
+		acceleration = swimming_force_vector / body_mass
+		speed += acceleration * delta
+		position += speed * delta
+
+# The following variables are updated regularly in _physics_process
+var closest_fish = null
+var baricenter = Vector3.ZERO # computed with equation 5
+var mean_velocity = Vector3.ZERO # computed with equation 7
+var closest_food = null
+
+func compute_swimming_force():
+	# the equations here are described by Figure 4
+	# and equations 2 to 10
+	var closest_boundary = find_closest_boundary()
+
+	var separation = Vector3.ZERO
+	if closest_fish != null:
+		separation = w[0] * (position - closest_fish.position).normalized()
+	
+	var cohesion = w[1] * (baricenter - position).normalized()
+	
+	var alignment = w[2] * (mean_velocity - speed).normalized()
+	
+	var boundary_avoidance = Vector3.ZERO
+	var distance_to_boundary = (position - closest_boundary).length()
+	if distance_to_boundary <  field_of_view():
+		boundary_avoidance = w[3] * (position - closest_boundary).normalized()
+	
+	var food_attraction = Vector3.ZERO
+	if closest_food != null:
+		food_attraction = w[4] * (closest_food.position - position).normalized()
+		
+	var random_vector = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1))
+	var random_movement = w[5] * random_vector.normalized()
+	
+	return (
+		separation +
+		cohesion +
+		alignment +
+		boundary_avoidance +
+		food_attraction +
+		random_movement
+	)
+
+func find_closest_boundary():
+	const TANK_RADIUS = 2.0
+	const TANK_HEIGHT = 1.0
+	var tank_center = Vector3(0, position.y, 0)
+	var vector_to_center = tank_center - position
+	var prolongation_to_boundary = vector_to_center.normalized() * TANK_RADIUS
+	var distance_to_border = (prolongation_to_boundary - position).length()
+	var distance_to_top = abs(TANK_HEIGHT - position.y)
+	var distance_to_bottom = position.y
+	if distance_to_border < distance_to_top && distance_to_border < distance_to_bottom:
+		return prolongation_to_boundary
+	elif distance_to_top < distance_to_bottom:
+		return Vector3(position.x, TANK_HEIGHT, position.z)
+	else:
+		return Vector3(position.x, 0, position.z)
+
+func start_feeding():
+	current_phase = PHASE.FEEDING
+	feed_intake_weight = 0.0
+	
+func feed_in_tank():
+	return get_tree().get_nodes_in_group("Food").size() > 0
+
+func _physics_process(_delta):
+	var school = get_tree().get_nodes_in_group("School")
+	var smallest_distance = INF
+	var visible_fishes = 0
+	
+	closest_fish = null
+	baricenter = Vector3.ZERO
+	mean_velocity = Vector3.ZERO
+	
+	for fish in school:
+		if fish == self:
+			continue
+		var distance = (fish.position - position).length()
+		if distance <  field_of_view():
+			visible_fishes += 1
+			baricenter += fish.position
+			mean_velocity += fish.speed
+			if distance < smallest_distance:
+				smallest_distance = distance
+				closest_fish = fish
+	
+	if visible_fishes > 0:
+		baricenter /= visible_fishes
+		mean_velocity /= visible_fishes
+	
+	closest_food = null
+	var foods = get_tree().get_nodes_in_group("Food")
+	smallest_distance = INF
+	for food in foods:
+		var distance = (food.position - position).length()
+		if distance < field_of_view() && distance < smallest_distance:
+			smallest_distance = distance
+			closest_food = food
+
+# TODO: replace it with a function that takes a point
+# to take into account the dead space at the back of the
+# fish
+func field_of_view():
+	return 2 * total_length
---
+++ Main.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@export var food_scene: PackedScene
+@export var fish_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _input(event):
+	if event.is_action("spawn_food"):
+		var food = food_scene.instantiate()
+		food.position = Vector3(0, 0.5, 0)
+		add_child(food)
+	if event.is_action("spawn_fish"):
+		var fish = fish_scene.instantiate()
+		var rangle = randf_range(-PI, PI)
+		fish.position = Vector3(
+			cos(rangle) * randf_range(0, 1.8),
+			randf_range(0, 1),
+			sin(rangle) * randf_range(0, 1.8),
+		)
+		add_child(fish)
