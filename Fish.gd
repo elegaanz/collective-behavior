14e0e54d253e063008cabd4d675d8a20acb1c217
Ana Gelez <ana@gelez.xyz>
2023-11-10 15:15:21 +0100

    Finish the implementation of the simulation

--- Fish.gd
+++ Fish.gd
@@ -1,12 +1,11 @@
 extends Node3D
 
 # W in the article
-var body_mass = 1.0
+var body_mass = 0.03853
 # TL in the article
 var total_length = 0.1
 # S in the article
 var feed_intake_weight = 0.0
-const MAX_FEED_INTAKE = 10.0 # TODO: actual value
 
 var swimming_force_vector = Vector3.RIGHT
 var acceleration = Vector3.RIGHT
@@ -17,12 +16,17 @@ enum PHASE {
 	GROWING,
 }
 
+var phase_duration = 0.0
+const FEEDING_PHASE_DURATION = 10.0
 var current_phase = PHASE.FEEDING
 var mesh: MeshInstance3D
+
+var current_iteration = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mesh = get_node("MeshInstance3D")
+	scale_mesh()
 
 var w = [0.6, 0.4, 0.4, 1.0, 0.0, 0.2] # w1 to w6 (the index are shifted in Godot and go from 0 to 5)
 var max_speed_coeff = 1.5 # Cv in the article
@@ -31,7 +35,8 @@ var max_speed_coeff = 1.5 # Cv in the article
 func _process(delta):
 	if current_phase == PHASE.FEEDING:
 		# this algorithm is based on figure 2
-		if feed_in_tank() && feed_intake_weight < MAX_FEED_INTAKE:
+		var max_feed_intake = 0.04 * body_mass
+		if feed_in_tank() && feed_intake_weight < max_feed_intake:
 			w[4] = 3.0
 			max_speed_coeff = 7.0
 		else:
@@ -43,6 +48,9 @@ func _process(delta):
 		# Equations (1) in the article
 		acceleration = swimming_force_vector / body_mass
 		speed += acceleration * delta
+		var max_speed = max_speed_coeff * total_length
+		if speed.length() > max_speed:
+			speed = speed.normalized() * max_speed
 		var new_position = position + speed * delta
 		if inside_of_tank(new_position):
 			# if there was a significant move, rotate the fish towards the point
@@ -55,13 +63,35 @@ func _process(delta):
 			# This condition is rarely met, but it still exists
 			# as a security measure to make sure no fish escapes
 			# from the tank
-			var center_of_tank = Vector3(0, position.y, 0)
+			var center_of_tank = Vector3(0, 0.5, 0)
 			position += (center_of_tank - position).normalized() * delta * delta
+			speed /= 2
+
+		var food_in_contact: Node = get_food_in_contact()
+		if food_in_contact != null:
+			# We don't have a counter for how many food pellets were eaten
+			# (aka N_f^i), we just sum the weights as they come
+			feed_intake_weight += food_in_contact.weight
+			food_in_contact.get_parent().remove_child(food_in_contact)
+
+		phase_duration += delta
+		if phase_duration > FEEDING_PHASE_DURATION:
+			start_growth()
+	else:
+		var feed_conversion_efficiency = 1.0
+		var body_mass_gain = feed_intake_weight * feed_conversion_efficiency
+		body_mass += body_mass_gain
+
+		scale_mesh()
+
+		current_iteration += 1
+		print("iteration n° ", current_iteration)
+		start_feeding()
 
 # The following variables are updated regularly in _physics_process
 var closest_fish = null
-var baricenter = Vector3.ZERO # computed with equation 5
-var mean_velocity = Vector3.ZERO # computed with equation 7
+var baricenter = null # computed with equation 5
+var mean_velocity = null # computed with equation 7
 var closest_food = null
 
 func compute_swimming_force():
@@ -73,9 +103,13 @@ func compute_swimming_force():
 	if closest_fish != null:
 		separation = w[0] * (position - closest_fish.position).normalized()
 	
-	var cohesion = w[1] * (baricenter - position).normalized()
+	var cohesion = Vector3.ZERO
+	if baricenter != null:
+		cohesion = w[1] * (baricenter - position).normalized()
 	
-	var alignment = w[2] * (mean_velocity - speed).normalized()
+	var alignment = Vector3.ZERO
+	if mean_velocity != null:
+		alignment = w[2] * (mean_velocity - speed).normalized()
 	
 	var tank_top = Vector3(position.x, TANK_HEIGHT, position.z)
 	var tank_floor = Vector3(position.x, 0, position.z)
@@ -114,19 +148,31 @@ func find_closest_boundary():
 func start_feeding():
 	current_phase = PHASE.FEEDING
 	feed_intake_weight = 0.0
-	
+	phase_duration = 0.0
+
+func start_growth():
+	current_phase = PHASE.GROWING
+
 func feed_in_tank():
 	return get_tree().get_nodes_in_group("Food").size() > 0
+	
+func get_food_in_contact():
+	var all_food = get_tree().get_nodes_in_group("Food")
+	for food in all_food:
+		var distance = (food.position - position).length()
+		if distance < speed.length() / 8:
+			return food
+	return null
 
 func _physics_process(_delta):
 	var school = get_tree().get_nodes_in_group("School")
 	var smallest_distance = INF
 	var visible_fishes = 0
-	
+
 	closest_fish = null
 	baricenter = Vector3.ZERO
 	mean_velocity = Vector3.ZERO
-	
+
 	for fish in school:
 		if fish == self:
 			continue
@@ -138,11 +184,14 @@ func _physics_process(_delta):
 			if distance < smallest_distance:
 				smallest_distance = distance
 				closest_fish = fish
-	
+
 	if visible_fishes > 0:
 		baricenter /= visible_fishes
 		mean_velocity /= visible_fishes
-	
+	else:
+		baricenter = null
+		mean_velocity = null
+
 	closest_food = null
 	var foods = get_tree().get_nodes_in_group("Food")
 	smallest_distance = INF
@@ -156,10 +205,16 @@ func _physics_process(_delta):
 # to take into account the dead space at the back of the
 # fish
 func field_of_view():
-	return 2 * total_length
+	return 0.1 * total_length
 
 func inside_of_tank(point):
 	var radius = Vector2(point.x, point.z)
 	return (radius.length() < TANK_RADIUS * 0.95 &&
 		0.1 < point.y &&
 		point.y < TANK_HEIGHT * 0.9)
+
+func scale_mesh():
+	const A = 0.0209
+	const B = 2.483
+	total_length = (body_mass / A)**(1 / B)
+	mesh.scale = 0.02 * total_length**1.2 * Vector3.ONE
