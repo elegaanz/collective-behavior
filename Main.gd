4a41a9b04bf42bafb3466ea3223f5156cbf69c0f
Ana Gelez <ana@gelez.xyz>
2023-12-24 14:24:31 +0100

    Add some UI

--- Main.gd
+++ Main.gd
@@ -9,8 +9,11 @@ var fish_data = []  # List to store fish data
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var popup: PopupMenu = get_node("HFlowContainer/ShapeMenu").get_popup()
+	popup.connect("id_pressed", _on_shape_menu)
+	
+	popup = get_node("HFlowContainer/SizeMenu").get_popup()
+	popup.connect("id_pressed", _on_size_menu)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -18,8 +21,8 @@ func _process(_delta):
 
 const TANK_RADIUS = 2
 
-const FOOD_SPAWN_AREA_SIZE = 1.0  #size of food distribution square
-const FEEDING_SHAPE = 0
+var feed_area_size = 1.0  #size of food distribution square
+var feeding_shape = 0
 # 0 for square
 # 1 for line
 # 2 for cross
@@ -31,7 +34,7 @@ var totalFishMass = 0.0 # total fish mass
 func _input(event):
 	if event.is_action_released("spawn_food"):
 		spawn_food()
-	if event.is_action("spawn_fish"):
+	if event.is_action_released("spawn_fish"):
 		var fish = fish_scene.instantiate()
 		var rangle = randf_range(-PI, PI)
 		fish.position = Vector3(
@@ -48,7 +51,7 @@ func _input(event):
 			top_cam.current = true
 		else:
 			cam.current = true
-	if event.is_action_pressed("save_fish"):
+	if event.is_action_released("save_fish"):
 		var school = get_tree().get_nodes_in_group("School")
 		var last_assigned_id = 0
 		for fish in school:
@@ -64,19 +67,19 @@ func _input(event):
 
 func spawn_food():
 	
-	match FEEDING_SHAPE :
+	match feeding_shape :
 	# for a squared distribution
 		0: 
 			var foodCount = int(totalFishMass * FOOD_MASS_PERCENTAGE / 0.065)
 			if foodCount == 0:
 				var food = food_scene.instantiate()
-				food.position = Vector3(randf_range(-FOOD_SPAWN_AREA_SIZE / 2, FOOD_SPAWN_AREA_SIZE / 2), 0.5, randf_range(-FOOD_SPAWN_AREA_SIZE / 2, FOOD_SPAWN_AREA_SIZE / 2))
+				food.position = Vector3(randf_range(-feed_area_size / 2, feed_area_size / 2), 0.5, randf_range(-feed_area_size / 2, feed_area_size / 2))
 				add_child(food)
 				totalFishMass += food.weight
 			else :
 				for _i in range(floor(foodCount)): 
 					var food = food_scene.instantiate()
-					food.position = Vector3(randf_range(-FOOD_SPAWN_AREA_SIZE / 2, FOOD_SPAWN_AREA_SIZE / 2), 0.5, randf_range(-FOOD_SPAWN_AREA_SIZE / 2, FOOD_SPAWN_AREA_SIZE / 2))
+					food.position = Vector3(randf_range(-feed_area_size / 2, feed_area_size / 2), 0.5, randf_range(-feed_area_size / 2, feed_area_size / 2))
 					add_child(food)
 					totalFishMass += food.weight
 	
@@ -85,13 +88,13 @@ func spawn_food():
 			var foodCount = int(totalFishMass * FOOD_MASS_PERCENTAGE / 0.065)
 			if foodCount == 0:
 				var food = food_scene.instantiate()
-				food.position = Vector3(randf_range(-FOOD_SPAWN_AREA_SIZE / 2, FOOD_SPAWN_AREA_SIZE / 2), 0.5, 0.0)
+				food.position = Vector3(randf_range(-feed_area_size / 2, feed_area_size / 2), 0.5, 0.0)
 				add_child(food)
 				totalFishMass += food.weight
 			else :
 				for _i in range(floor(foodCount)): 
 					var food = food_scene.instantiate()
-					food.position = Vector3(randf_range(-FOOD_SPAWN_AREA_SIZE / 2, FOOD_SPAWN_AREA_SIZE / 2), 0.5, 0.0)
+					food.position = Vector3(randf_range(-feed_area_size / 2, feed_area_size / 2), 0.5, 0.0)
 					add_child(food)
 					totalFishMass += food.weight
 			
@@ -99,11 +102,11 @@ func spawn_food():
 		2:
 			var foodCount = int(totalFishMass * FOOD_MASS_PERCENTAGE / 0.065)
 			#generate two lines
-			var start_horizontal = Vector3(-FOOD_SPAWN_AREA_SIZE / 2, 0.5, 0.0)
-			var end_horizontal = Vector3(FOOD_SPAWN_AREA_SIZE / 2, 0.5, 0.0)
+			var start_horizontal = Vector3(-feed_area_size / 2, 0.5, 0.0)
+			var end_horizontal = Vector3(feed_area_size / 2, 0.5, 0.0)
 
-			var start_vertical = Vector3(0.0, 0.5, -FOOD_SPAWN_AREA_SIZE / 2)
-			var end_vertical = Vector3(0.0, 0.5, FOOD_SPAWN_AREA_SIZE / 2)
+			var start_vertical = Vector3(0.0, 0.5, -feed_area_size / 2)
+			var end_vertical = Vector3(0.0, 0.5, feed_area_size / 2)
 			
 			if foodCount == 0:
 				var food = food_scene.instantiate()
@@ -124,21 +127,19 @@ func spawn_food():
 					totalFishMass += food.weight
 			
 	# around the tank
-		3:
-			var OUTER_RADIUS = 2.0
-			
+		3:			
 			var foodCount = int(totalFishMass * FOOD_MASS_PERCENTAGE / 0.065)
 			if foodCount == 0:
 				var food = food_scene.instantiate()
 				var angle = randf_range(0, 2 * PI)
-				food.position = Vector3(OUTER_RADIUS * cos(angle), 0.5, OUTER_RADIUS * sin(angle))
+				food.position = Vector3(feed_area_size * cos(angle), 0.5, feed_area_size * sin(angle))
 				add_child(food)
 				totalFishMass += food.weight
 			else :
 				for _i in range(floor(foodCount)): 
 					var food = food_scene.instantiate()
 					var angle = randf_range(0, 2 * PI)
-					food.position = Vector3(OUTER_RADIUS * cos(angle), 0.5, OUTER_RADIUS * sin(angle))
+					food.position = Vector3(feed_area_size * cos(angle), 0.5, feed_area_size * sin(angle))
 					add_child(food)
 					totalFishMass += food.weight
 
@@ -156,3 +157,27 @@ func save_fish_data_to_csv():
 		file.store_line(line)    
 	file.close()
 	print("Fish data saved successfully.")
+
+func _on_shape_menu(id):
+	name = "square"
+	if id == 1:
+		name = "line"
+	elif id == 2:
+		name = "cross"
+	elif id == 3:
+		name = "circle"
+	
+	get_node("HFlowContainer/ShapeMenu").text = "Feeding shape: " + name
+	feeding_shape = id
+
+func _on_size_menu(id):
+	feed_area_size = 1.0 + (0.5 * id)	
+	get_node("HFlowContainer/SizeMenu").text = "Feeding area size: " + str(feed_area_size)
+
+func _on_spawn_button_pressed():
+	spawn_food()
+
+func _on_camera_button_pressed():
+	var evt = InputEventAction.new()
+	evt.action = "switch_camera"
+	Input.parse_input_event(evt)
